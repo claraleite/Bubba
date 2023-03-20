373ab2b2ecbd03a98afce440cf4d3a968ea1bede
claraleite <mccl@cin.ufpe.br>
2023-03-20 11:13:46 -0300

    adding xcode project

---
+++ Bubba/Bubba/BubbaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbaApp.swift
+//  Bubba
+//
+//  Created by mccl on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BubbaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
