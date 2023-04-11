4ad908af0d6bd94ac781230ee96137a9a15c8d19
claraleite <mccl@cin.ufpe.br>
2023-04-11 19:09:46 -0300

    hotfix/placing-bubba: placing bubba on garden and home views

--- Bubba/Bubba/HomeView.swift
+++ Bubba/Bubba/HomeView.swift
@@ -12,6 +12,11 @@ struct HomeView: View {
     @State var isPresentingRoomView: Bool = false
     @State var isPresentingGardenView: Bool = false
     
+    @State private var location: CGPoint = CGPoint(x: UIScreen.main.bounds.width * 0.6, y: UIScreen.main.bounds.height * 0.75)
+    @State var isDragging = false
+    
+    @State var shouldAnimate: Bool = false
+    
     var body: some View {
         
         GeometryReader { geometry in
@@ -33,6 +38,31 @@ struct HomeView: View {
                             HomeButton(icon: Image("casa-07"), nextView: Garden())
                         }
                     }
+                
+                        
+                        Image("bubba")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: geometry.size.width * 0.2, height: geometry.size.height * 0.3)
+                            .position(location)
+                            .scaleEffect(shouldAnimate ? 1.07 : 1.05)
+                            .animation(Animation.easeInOut(duration: 0.7).repeatForever(autoreverses: true), value: shouldAnimate)
+                            .onAppear() {
+                                self.shouldAnimate = true
+                                
+                            }
+                            .gesture(
+                                DragGesture().onChanged { value in
+                                    location = value.location
+                                    isDragging = true
+                                }
+                                    .onEnded { value in
+                                        withAnimation(.spring()) {
+                                            isDragging = false
+                                            location = CGPoint(x: location.x, y: geometry.size.height * 0.8)
+                                        }
+                                    }
+                                )
                     
                     
                 }
